0a70d1d047a4576641de813e5ea976d1f9e778e8
Eric Mellow <ericmellow@gmail.com>
2018-06-26 14:30:37 -0600

    Add post function for creating new projects to server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ app.use(express.static('public'));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
+app.locals.projects = [{id: 1, name: 'Project Name', palettes: {}}]
 
 app.get('/', (request, response) => {
   response.send('Go ahead and pick those palettes!');
@@ -13,3 +14,18 @@ app.get('/', (request, response) => {
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+app.post('/api/v1/projects', (response, request) => {
+  const id = Date.now().toString();
+  const { name } = request.body;
+  const nameAlreadyExists = app.locals.projects.find(project => project.name === name)
+
+  if (nameAlreadyExists) {
+    response.status(304).send({
+      error: 'Project name already exists.'
+    })
+  } else {
+    app.locals.projects.push({id, name, palettes: {}})
+    response.status(201).json({id, name, palettes: {}})
+  }
+})
