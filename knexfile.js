171f0d8b690bc3e23ff8a45edc01f77e51a74c59
Eric Mellow <ericmellow@gmail.com>
2018-06-28 15:05:03 -0600

    Set up testing

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,16 @@
 // Update with your config settings.
 
 module.exports = {
-
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/palette',
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/dev'
+    }
+  },
   development: {
     client: 'pg',
     connection: 'postgres://localhost/palette',
---
+++ test/routes.spec.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const knex = require('../db/knex');
+
+chai.use(chaiHttp);
+
+describe('CLIENT routes', () => {
+  
+
+  it('should return a 404 for a route that does not exist', done => {
+    chai.request(server)
+      .get('/sad')
+      .end((error, response) => {
+        response.should.have.status(404);
+        done();
+      })
+  })
+
+})
+
+describe('API routes', () => {
+  
+  })
