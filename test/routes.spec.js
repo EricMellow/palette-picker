a1b65d6b9822d523015f108e47eee7e0bd20156c
Eric Mellow <ericmellow@gmail.com>
2018-06-28 23:11:16 -0600

    Add GET test for palettes endpoint

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -95,6 +95,7 @@ describe('API routes', () => {
       .end((error, response) => {
         response.should.have.status(200)
         response.body.should.be.a('array')
+        response.body.length.should.equal(1);
         response.body[0].should.have.a.property('name')
         response.body[0].name.should.equal('Mayhem')
         done()
@@ -138,4 +139,46 @@ describe('API routes', () => {
     });
     });
   
+  describe('GET /api/v1/palettes', () => {
+    beforeEach(function (done) {
+      knex.migrate.rollback()
+        .then(function () {
+          knex.migrate.latest()
+            .then(function () {
+              return knex.seed.run()
+                .then(function () {
+                  done();
+                });
+            });
+        });
+    });
+
+    it('should return an array of palettes', done => {
+      chai.request(server)
+        .get('/api/v1/palettes')
+        .end((error, response) => {
+          response.should.have.status(200)
+          response.body.should.be.a('array')
+          response.body.length.should.equal(2);
+          response.body[0].should.have.a.property('name')
+          response.body[0].name.should.equal('First Palette')
+          response.body[0].should.have.a.property('color1')
+          response.body[0].color1.should.equal('rgba(1, 2, 3)')
+          response.body[0].should.have.a.property('color2')
+          response.body[0].color2.should.equal('rgba(2, 3, 4)')
+          response.body[0].should.have.a.property('color3')
+          response.body[0].color3.should.equal('rgba(3, 4, 5)')
+          response.body[0].should.have.a.property('color4')
+          response.body[0].color4.should.equal('rgba(4, 5, 6)')
+          response.body[0].should.have.a.property('color5')
+          response.body[0].color5.should.equal('rgba(9, 8, 7)')
+          response.body[0].should.have.a.property('project_id')
+          response.body[0].project_id.should.equal(1)
+          done()
+        })
+    });
+  });
+
+
+
   })
